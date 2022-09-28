15d5ac516aaf138b5369c1973a8e9ca204193445
BoonyaOnkling <boonyanutch.133@gmail.com>
2022-09-28 11:34:00 +0700

    28/9/22

--- App.js
+++ App.js
@@ -1,50 +1,100 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Button, TextInput, StyleSheet,Image,SafeAreaView } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { 
+  createDrawerNavigator, 
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+import React from 'react';
+import ProductScreen from './screens/ProductScreen';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <Button
-      title="GO TO SETTING"
-      onPress={()=> navigation.navigate('Setting')}
-      />
-    </View>
-  );
+const MyTheme ={
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    primary:'rgb(255,45,5)'
+  }
 }
 
-function SettingScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Setting!</Text>
-      <Button
-      title="GO TO Home"
-      onPress={()=> navigation.goBack()}
-      />
+function FeedScreen({navigation}){
+  return(
+    <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
+        <Text>Feed Screen</Text>
+        <Button title='Open drawer' onPress={()=> navigation.openDrawer()}/>
     </View>
-  );
+  )
 }
 
-const Tab = createBottomTabNavigator();
+function NotificationScreen(){
+  return(
+    <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
+      <Text>Notification Screen</Text>
+    </View>
+  )
+}
 
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
-    </Tab.Navigator>
+
+
+function CustomDrawerContent(props){
+  return(
+    <SafeAreaView>
+      <Image
+        source={require('./assets/react_logo.png')}
+        style={styles.sideMenuProfileIcon}
+     />
+    <DrawerContentScrollView {...props}>
+     <DrawerItemList {...props}/>
+     <DrawerItem 
+     label="Close Drawer"
+     onPress={() => props.navigation.closeDrawer()}
+     />
+    </DrawerContentScrollView>
+    </SafeAreaView>
+  )
+}
+
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer(){
+  return(
+      <Drawer.Navigator 
+      useLegacyImplementation
+      drawerContent={(props)=><CustomDrawerContent {...props}/>}
+      screenOptions={{
+        drawerStyle:{
+          // backgroundColor:'#F6E4FF',
+          width:240
+        }
+      }}
+      >
+        <Drawer.Screen name='Home' component={HomeScreen}/>
+        <Drawer.Screen name='Product' component={ProductScreen}/>
+      </Drawer.Navigator>
   );
 }
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MyTabs />
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer/>
     </NavigationContainer>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: 'center',
+  },
+})
---
+++ AppSetting.js
@@ -0,0 +1,50 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+      <Button
+      title="GO TO SETTING"
+      onPress={()=> navigation.navigate('Setting')}
+      />
+    </View>
+  );
+}
+
+function SettingScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting!</Text>
+      <Button
+      title="GO TO Home"
+      onPress={()=> navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Setting" component={SettingScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+};
+
+export default App;
